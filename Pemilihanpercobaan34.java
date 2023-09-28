2a85d6845dd9e3bdd6e487517d9859f1af4ad711
anandarahmawati24 <rahmawatiananda571@gmail.com>
2023-09-28 10:44:28 +0700

    modifikasi kode program

--- Pemilihanpercobaan34.java
+++ Pemilihanpercobaan34.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
-import java.util.scanner;
 public class Pemilihanpercobaan34{
-
 public static void main(String[] args) {
+    Scanner input04= new Scanner(System.in);
     double angka1, angka2, hasil;
     char operator;
-   Scanner input04= new Scanner(System.in);
    System.out.print("Masukkan angka pertama:   ");
    angka1=input04.nextDouble();
    System.out.print("Masukkan angka kedua:   ");
@@ -13,22 +11,22 @@ public static void main(String[] args) {
    System.out.print("Masukkan operator ( + - * /):   ");
    operator= input04.next().charAt(0);
    switch (operator) {
-    case '+':
-    hasil= angka1 + angka2;
-    System.out.println(angka1 + " + " +angka2+ "=" +hasil);
+    case '+' :
+    hasil = angka1 + angka2;
+    System.out.println(angka1 + " + " + angka2 + " = " + hasil);   
+case '-':
+    hasil = angka1 - angka2;
+    System.out.println(angka1 + " - " + angka2 + " = " + hasil);
     break;
-    case '-':
-            hasil = angka1 - angka2;
-            System.out.println(angka1 + " - " + angka2 + " = " + hasil);
-            break;
-        case '*':
-            hasil = angka1 * angka2;
-            System.out.println(angka1 + " * " + angka2 + " = " + hasil);
-            break;
-        case '/':
-            hasil = angka1 / angka2;
-            System.out.println(angka1 + " / " + angka2 + " = " + hasil);
-            break;
+case '*':
+    hasil = angka1 * angka2;
+    System.out.println(angka1 + " * " + angka2 + " = " + hasil);
+    break;
+case '/':
+    hasil = angka1 / angka2;
+    System.out.println(angka1 + " / " + angka2 + " = " + hasil);
+    break;
    }
+   
 }
 }
